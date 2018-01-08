eef791b452e8758ae76adc19adbda9337f2e31f7
tuananhpham94 <tuananh191194@gmail.com>
2018-01-08 20:22:36 +1300

    allow see data

--- app/Http/Controllers/User/IndexController.php
+++ app/Http/Controllers/User/IndexController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Kiwifruit;
+use App\ScannedItem;
+use App\TempLamb;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('pages.user.index', [
+        $kiwifruits = Kiwifruit::all();
+        $scannedItems = ScannedItem::all();
+        $tempLambs = TempLamb::all();
+        return view('pages.user.index', [ 'kiwifruits' => $kiwifruits, 'scannedItems' => $scannedItems,'tempLambs' => $tempLambs
         ]);
     }
 }
--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -4,51 +4,98 @@
 @stop
 
 @section('styles')
-@parent
-<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-<style>
-    html, body {
-        height: 100%;
-    }
+    @parent
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <style>
+        html, body {
+            height: 100%;
+        }
 
-    body {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        display: table;
-        font-weight: 100;
-        font-family: 'Lato';
-    }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
 
-    .container {
-        text-align: center;
-        display: table-cell;
-        vertical-align: middle;
-    }
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
 
-    .content {
-        text-align: center;
-        display: inline-block;
-    }
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
 
-    .title {
-        font-size: 96px;
-    }
-</style>
+        .title {
+            font-size: 96px;
+        }
+    </style>
 @stop
 
 @section('title')
 @stop
 
 @section('scripts')
-@parent
+    @parent
 @stop
 
 @section('content')
-<div class="container">
-    <p class="content">
+    <div class="container">
+        <p class="content">
         <div class="title">Spectrometer Data</div>
-        <p><a href="/signin">Log in</a></p>
+        @if(\Illuminate\Support\Facades\Auth::check())
+            @foreach($kiwifruits as $kiwifruit)
+                <h1>{!! $kiwifruit->name !!}</h1>
+                <table>
+                    <tr>
+                        <th>Sample</th>
+                        <th>Area</th>
+                        <th>Scan</th>
+                        <th>Calories</th>
+                        <th>Carbs</th>
+                        <th>Water</th>
+                    </tr>
+                    @foreach($kiwifruit->kiwifruitscanned as $scan)
+                        <tr>
+                            <td>{!! $scan->sample !!}</td>
+                            <td>{!! $scan->area !!}</td>
+                            <td>{!! $scan->scan !!}</td>
+                            <td>{!! $scan->calories !!}</td>
+                            <td>{!! $scan->carbs !!}</td>
+                            <td>{!! $scan->water !!}</td>
+                        </tr>
+                    @endforeach
+                </table>
+            @endforeach
+            <h1>NIRScan product</h1>
+            @foreach($scannedItems as $scannedItem)
+                <h2>{!! $scannedItem->name !!}</h2>
+                <img width="40%" src="{!! $scannedItem->image !!}">
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Link download</th>
+                        <th>View online</th>
+                    </tr>
+                    @foreach($tempLambs->where('scanned_item_id', $scannedItem->id) as $tempLamb)
+
+                        <tr>
+                            <td>{!! $tempLamb->name !!}</td>
+                            <td><a href="{{ $tempLamb->excel_file }}">Download</a></td>
+                            <td><a href="{{ asset($tempLamb->view_online) }}">View online</a></td>
+                        </tr>
+                    @endforeach
+
+                </table>
+            @endforeach
+        @else
+            <p><a href="/signin">Log in</a></p>
+        @endif
     </div>
-</div>
+    </div>
 @stop
