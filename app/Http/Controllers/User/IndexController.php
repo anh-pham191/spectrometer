5b10fa81849a65ae61ada736aa7b10814530cddd
tuananhpham94 <tuananh191194@gmail.com>
2018-03-27 21:25:25 +1300

    fix

--- app/Http/Controllers/User/IndexController.php
+++ app/Http/Controllers/User/IndexController.php
@@ -162,12 +162,7 @@ class IndexController extends Controller
         Mail::send('pages.user.mail', ['subject' => $subject, 'name' => $name, 'mess' => $mess , 'phone' => $phone, 'email' => $email], function ($message) {
             $message->to('tuananh191194@gmail.com')->subject('New contact submission from nir-data.com');
         });
-//        $message = 'You got a new contact from www.nir-data.com by ' . $contact->name . ' with phone number of ' . $contact->phone . ' and the email address appeared as '
-//        . $contact->email . '. Please reply to this email to send email address to the person who contacts. Below is the message'."\r\n" .$contact->message;
-//        $headers = 'From: <webmaster@example.com>'. "\r\n" .
-//            'Reply-To: ' . $contact->email . "\r\n" .
-//            'X-Mailer: PHP/' . phpversion();
-//        mail($to, $subject, $message, $headers);
+//       
         return view('pages.user.getcontact');
 
     }
